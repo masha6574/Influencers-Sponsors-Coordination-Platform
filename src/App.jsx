34965e435e396d72a15ece498decfe75026a8b13
masha6574 <smadhavsharma513@gmail.com>
2025-02-12 18:09:33 +0530

    Initial commit with Vite React setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './index.css'
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-[2rem] bg-red-500 text-xl text-white">Hello World!!!</h1>
+    </div>
+  )
+}
+
+export default App;
