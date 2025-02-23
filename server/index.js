8c5039c56dbb951be8f80b7d5d53239d8c84cfe3
masha6574 <smadhavsharma513@gmail.com>
2025-02-23 18:43:37 +0530

    update dotenv

--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
+require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const InfluencerModel = require('./models/Influencer');
-
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb+srv://masha6574:masha%40masha2006@cluster0.f8nww.mongodb.net/influencer", {
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 }).then(() => console.log("Connected to MongoDB Atlas"))
     .catch(err => console.error("MongoDB Atlas Connection Error:", err));
 
